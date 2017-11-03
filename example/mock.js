bea87cbf69c9fcdb72a374095f1b20a5859b9108
ziaochina <ziaochina@gmail.com>
2017-11-03 10:49:19 +0800

    zlj

---
+++ example/mock.js
@@ -0,0 +1,9 @@
+
+import './apps/mk-app-dashboard-analysis/apps/mk-app-hot-search-widget/mock.js';
+import './apps/mk-app-dashboard-analysis/apps/mk-app-market-widget/mock.js';
+import './apps/mk-app-dashboard-analysis/apps/mk-app-sale-proportion-widget/mock.js';
+import './apps/mk-app-dashboard-analysis/apps/mk-app-sale-trend-widget/mock.js';
+import './apps/mk-app-dashboard-analysis/apps/mk-app-sale-widget/mock.js';
+import './apps/mk-app-dashboard-analysis/apps/mk-app-trade-widget/mock.js';
+import './apps/mk-app-dashboard-analysis/apps/mk-app-visit-widget/mock.js';
+import './apps/mk-app-dashboard-analysis/mock.js';
